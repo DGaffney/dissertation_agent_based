9cb3188883f3e1c122997ddf778ed9eedc2d2534
Peat Bakke <peat@peat.org>
2017-05-11 16:43:42 -0700

    Update peat.rb

--- Sandbox/peat.rb
+++ Sandbox/peat.rb
@@ -1,8 +1,10 @@
-# WORLD is a hash of "subreddit" => ["outbound_subreddit", ...]
-# STATS is a hash of "subreddit" => { attribute: value }
-# WALKERS is a list of { username: "derpderp", starting_sub: "some_subreddit", post_count: 10 }
-# HISTORY is a hash of "username" => ["subreddit", ...]
+# WORLD is static global hash of "subreddit" => ["outbound_subreddit", ...]
+# STATS is a static global hash of "subreddit" => { attribute: value }
+# WALKERS is a static global list of { username: "derpderp", starting_sub: "some_subreddit", post_count: 10 }
+# HISTORY is a static global hash of "username" => ["subreddit", ...]
 # RESULTS is a hash of "username" => ["subreddit", ...]
+
+
 
 WALKERS.each do |walker|
   RESULTS[walker[:username]] = run_walk(WORLD, STATS, walker)
