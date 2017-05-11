cbfc0fb1a6319b3fda22726a2333d8ce9a614705
Peat Bakke <peat@peat.org>
2017-05-11 16:39:00 -0700

    Update peat.rb

--- Sandbox/peat.rb
+++ Sandbox/peat.rb
@@ -2,3 +2,8 @@
 # STATS is a hash of "subreddit" => { attribute: value }
 # WALKERS is a list of { username: "derpderp", starting_sub: "some_subreddit", post_count: 10 }
 # HISTORY is a hash of "username" => ["subreddit", ...]
+# RESULTS is a hash of "username" => ["subreddit", ...]
+
+WALKERS.each do |walker|
+  HISTORY[walker[:username]] = run_walk(WORLD, STATS, walker)
+end
