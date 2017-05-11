b8149fd4435f108f6a17755c4c81e45ba732ea51
Peat Bakke <peat@peat.org>
2017-05-11 16:24:21 -0700

    Update peat.rb

--- Sandbox/peat.rb
+++ Sandbox/peat.rb
@@ -1,3 +1,4 @@
 # WORLD is a hash of "subreddit" => ["outbound_subreddit", ...]
 # STATS is a hash of "subreddit" => { attribute: value }
-# WALKERS is a list of { nick: ... }
+# WALKERS is a list of { nick: ..., starting_sub: ... }
+# HISTORY is a hash of "username" => ["subreddit", ...]
